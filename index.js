fca05fb04e76e360a6cedb71b5e0f6e74cb502b6
Marcelle Vukoja <booksread555@gmail.com>
2022-11-19 20:33:39 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
 // Write your solution here!
-
+/* cats.push("Kitty");*/
+/*console.log(cats);*/
 // This function should append a cat to the end of the array, destructively.
-destructivelyAppendCat(name) {
-
-}
+/*function destructivelyAppendCat(name) {
+    cats.push(name);
+}*/
 
 // this function should prepend a cat to the beginning of the cats array, destructively.
-destructivelyPrependCat(name) {
-
+/*function destructivelyPrependCat(name) {
+    cats.unshift(name);
 }
+cats.unshift("Kitty");
+console.log(cats);*/
 
 // this function should remove the last cat from the cats array, destructively.
-destructivelyRemoveLastCat() {
+/*cats.pop();
+console.log(cats);
 
-}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}*/
 
 // this function should remove the first cat from the cats array, destructively.
-destructivelyRemoveFirstCat() {
-
-}
+/*cats.shift();
+console.log(cats);
+/*function destructivelyRemoveFirstCat() {
+    cats.shift();
+}*/
 
 // this function should append a cat to the cats array and return a new array, leaving the cats array unchanged
-appendCat(name) {
+/*const copyOfCats = [...cats, "Kitty"];
+/*console.log(copyOfCats);*/
 
-}
+/*function copyOfCats(name) {
+    return [...cats, name];
+}*/
 
 // this function should prepend a cat to the cats array and return a new array, leaving the cats array unchanged
-prependCat(name) {
-
-}
-
+/*const prependCat = ["Kitty", ...cats];
+console.log(prependCat);*/
+/*function prependCat(name) {
+    return [name, ...cats];
+}*/
+    
 // this function should remove the last cat in the cats array and return a new array, leaving the cats array unchanged
-removeLastCat() {
-
-}
+/*cats.slice(0, cats.length - 1);
+console.log(cats)*/
+/*function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}*/
 
 // this function should remove the first cat from the cats array and return a new array, leaving the cats array unchanged
-removeFirstCat() {
-
-}
+cats.slice(1);
+console.log(cats.slice(1));
+/*function removeFirstCat() {
+    return cats.slice(1);
+}*/
